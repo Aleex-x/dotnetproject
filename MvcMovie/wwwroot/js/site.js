543e9731e915bbdb07c5e1f76767acd9deb01967
Alex <alex_goca50@hotmail.com>
2019-07-15 00:03:04 -0600

    add ajax method and class to return the information

--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = () => {
+  $.ajax({
+    url: '/byeworld/GetPlayers',
+    method: 'POST',
+    data: {
+      var: 'JESUCRISTOOOOOOOOOOOOOO',
+    },
+    success: (data) => {
+      console.log('success');
+      if (!!data) {
+        data.forEach(({user, pass, id}, index) => {
+          $(`.row .col-sm:nth-child(${index + 1})`).html(user);
+        });
+      }
+    },
+    error: (data) => {
+      console.log('error');
+      console.log(data);
+    },
+  });
+}
